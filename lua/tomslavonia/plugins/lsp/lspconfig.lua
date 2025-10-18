2505aff6c052015c52691f8c79289e7cf15a1002
Tom Slavonia <tomslavonia@Toms-MacBook-Pro.local>
2025-10-18 11:34:42 -0700

    remove failed arduino IDE fix

--- lua/tomslavonia/plugins/lsp/lspconfig.lua
+++ lua/tomslavonia/plugins/lsp/lspconfig.lua
@@ -56,20 +56,6 @@ return {
 
     vim.lsp.enable("tsserver")
 
-    lspconfig.arduino_language_server.setup({
-      cmd = {
-        "arduino-language-server",
-        "-clangd",
-        "C:/msys64/mingw64/bin/clangd.exe",
-        "-cli",
-        "C:/Program Files/Arduino CLI/arduino-cli.exe",
-        "-cli-config",
-        "C:/Users/tomslavonia/AppData/Local/Arduino15/arduino-cli.yaml",
-        "-fqbn",
-        "arduino:esp32:nano_nora",
-      },
-    })
-
     vim.lsp.enable("arduino_language_server")
 
     vim.lsp.config("basedpyright", {
